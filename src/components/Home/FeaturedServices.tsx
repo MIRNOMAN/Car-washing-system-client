97a779bd02d40ecd912fe9da5a330a69d7e19bf9
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-22 22:12:23 +0600

    handle slider useeffect

--- src/components/Home/FeaturedServices.tsx
+++ src/components/Home/FeaturedServices.tsx
@@ -11,6 +11,37 @@ const FeaturedServices = () => {
     const sliderRef = useRef<HTMLDivElement | null>(null);
     const sliderInstanceRef = useRef<KeenSliderInstance | null>(null);
     const { isError, isLoading, data } = useGetVehiclesQuery(undefined);
+
+    useEffect(() => {
+        if (sliderRef.current) {
+            sliderInstanceRef.current = new KeenSlider(
+                sliderRef.current,
+                {
+                    loop: true,
+                    slides: {
+                        origin: 'center',
+                        perView: 1.25,
+                        spacing: 16,
+                    },
+                    breakpoints: {
+                        '(min-width: 1024px)': {
+                            slides: {
+                                origin: 'auto',
+                                perView: 1.5,
+                                spacing: 32,
+                            },
+                        },
+                    },
+                },
+                []
+            );
+        }
+
+        return () => {
+            sliderInstanceRef.current?.destroy();
+        };
+    }, [data]);
+
   return (
     <div>FeaturedServices</div>
   )
