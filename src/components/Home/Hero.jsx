72fd57f3f68b45c3dc723971887fc69e281269e2
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-22 15:51:35 +0600

    crate banner left side

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -36,7 +36,34 @@ const Hero = () => {
     }, [currentSlider, sliders.length]);
 
   return (
-    <div>Hero</div>
+    <>
+            <div className="w-full h-96 sm:h-96 md:h-screen flex flex-col items-center justify-center gap-5 lg:gap-10 bg-cover bg-center before:absolute before:bg-black/50 before:inset-0 transform duration-1000 ease-linear"
+                style={{ backgroundImage: `url(${sliders[currentSlider].img})` }}>
+                {/* text container here */}
+
+                <div className="drop-shadow-lg text-white text-center w-full">
+
+                    <div className="relative flex justify-between mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8" >
+                        <div className="max-w-3xl text-center ltr:sm:text-left rtl:sm:text-right">
+                            {titles[currentSlider]}
+                            <p className="mt-4 max-w-lg  text-white md:text-left sm:text-xl/relaxed">
+                                Hit the road with RentNGo— your go-to car rental solution. Choose from a wide range of vehicles at unbeatable prices, with easy booking and 24/7 support. Enjoy flexible rentals and exclusive deals. Your journey starts here!"
+                            </p>
+
+                            <div className="mt-8 flex flex-wrap gap-4 text-center !justify-start md:justify-center">
+
+                                <Link to={`/vehicles`} className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto">
+                                    Book Now
+                                </Link>
+                            </div>
+                        </div>
+
+                        {/* filter */}
+                        {/* <HeroFilter /> */}
+                    </div>
+                </div>
+            </div>
+        </>
   )
 }
 
