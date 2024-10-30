87aecd95993fe16b75da69973c15701a9296688f
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-30 22:48:40 +0600

    all review rection

--- src/pages/Reviews.tsx
+++ src/pages/Reviews.tsx
@@ -1,6 +1,15 @@
+import SectionTitle from "@/components/reUsable/SectionTitle";
+import Loader from "@/components/shared/Loader";
+import { useGetAllRatingsQuery } from "@/redux/features/rating";
+import { RiDoubleQuotesL } from "react-icons/ri";
+import { RiDoubleQuotesR } from "react-icons/ri";
+import { FaStar } from "react-icons/fa";
+import { formatDateToDDMMYYYY } from "@/utils/utils";
+import { TReview } from "@/types";
 
 
 const Reviews = () => {
+  
   return (
     <div>Reviews</div>
   )
