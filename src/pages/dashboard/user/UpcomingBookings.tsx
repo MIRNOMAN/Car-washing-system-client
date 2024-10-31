b13b5ea8c5efbd2406bf9e592e8ef6a4cf9ef1f7
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-31 21:20:32 +0600

    create upcomming booking

--- src/pages/dashboard/user/UpcomingBookings.tsx
+++ src/pages/dashboard/user/UpcomingBookings.tsx
@@ -1,5 +1,7 @@
+import CountdownTimer from "../../../components/shared/CountdownTimer";
 import { useGetMyBookingQuery } from "../../../redux/features/booking/booking.api";
 import { TBooking } from "../../../types/booking.type";
+import { getTargetDateTime } from "../../../utils/utils";
 
 
 const UpcomingBookings = () => {
@@ -22,7 +24,47 @@ const UpcomingBookings = () => {
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error loading bookings</p>;
   return (
-    <div>UpcomingBookings</div>
+    <div>
+      <div className="text-center mb-5">
+        <h1 className="text-3xl font-bold text-rose-600 Oswald">Upcoming Bookings</h1>
+        <p className="text-xl mt-1 Montserrat">Overview of Upcoming Bookings</p>
+      </div>
+    <div className="grid lg:grid-cols-4 md:grid-cols-3 gap-5 grid-cols-1">
+      {futureBookings.map((booking) => {
+        const targetDateTime = getTargetDateTime(
+          booking.slot.date,
+          booking.slot.startTime
+        );
+        const desLength = booking?.service?.description.length;
+        // console.log("Target DateTime:", targetDateTime); // Debugging line
+        return (
+          <div
+            key={booking._id}
+            className="border p-4 rounded-lg shadow-md h-[210px] bg-primary-foreground/10"
+          >
+            <h2 className="text-xl font-semibold text-primary">
+              {booking.service.name}
+            </h2>
+            <p>
+              {booking.service.description.slice(0, 25)}
+              {desLength > 25 && "..."}
+            </p>
+            <p>Date: {new Date(booking.slot.date).toLocaleDateString()}</p>
+            <p>
+              Time: {booking.slot.startTime} - {booking.slot.endTime}
+            </p>
+            <p>
+              Vehicle: {booking.vehicleBrand} {booking.vehicleModel}
+            </p>
+            <div className="mt-2">
+              <p className="font-medium">Remaining:</p>
+              <CountdownTimer targetDateTime={targetDateTime} />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  </div>
   )
 }
 
