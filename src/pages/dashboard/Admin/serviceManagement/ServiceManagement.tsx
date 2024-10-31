04ee7032ae51e28383050e923c0d4e44c9875be9
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-31 22:11:36 +0600

    on sunmit service admin

--- src/pages/dashboard/Admin/serviceManagement/ServiceManagement.tsx
+++ src/pages/dashboard/Admin/serviceManagement/ServiceManagement.tsx
@@ -1,3 +1,7 @@
+import { toast } from "sonner";
+import { TErrorResponse, TService } from "../../../../types/redux.type";
+import { useState } from "react";
+import { useCreateServiceMutation, useDeleteServiceMutation, useGetServicesQuery, useUpdateServiceMutation } from "../../../../redux/features/services/services.api";
 
 type TInitialValues = {
   name: string;
@@ -14,6 +18,44 @@ const initialValues: TInitialValues = {
 };
 
 const ServiceManagement = () => {
+    // api
+    const { data, isLoading } = useGetServicesQuery(undefined);
+    const [serviceInfo] = useCreateServiceMutation();
+    const [deleteService] = useDeleteServiceMutation();
+    const [serviceData] = useUpdateServiceMutation();
+    // modal
+    const [isAddServiceModalOpen, setAddServiceModalOpen] = useState(false);
+    const [isServiceDeleteModalOpen, setServiceDeleteModalOpen] = useState(false);
+    const [isServiceUpdateModalOpen, setServiceUpdateModalOpen] = useState(false);
+    // utils
+    const [serviceID, setServiceId] = useState("");
+    const [serviceDetails, setServiceDetails] = useState<TService | null>(null);
+  
+    const serviceUpdateInitialValues = {
+      name: serviceDetails?.name || "",
+      description: serviceDetails?.description || "",
+      price: serviceDetails?.price || 0,
+      duration: serviceDetails?.duration || 0,
+    };
+  
+    const onSubmit = async (values: TInitialValues) => {
+      setAddServiceModalOpen(false);
+      const toastId = toast.loading("Service creating");
+      try {
+        const res = await serviceInfo(values).unwrap();
+        toast.success(res.message, { id: toastId, duration: 2000 });
+      } catch (error) {
+        console.log("error", error);
+        const err = error as TErrorResponse;
+        toast.error(err?.data?.errorMessages[0].message, {
+          id: toastId,
+          duration: 2000,
+        });
+      }
+    };
+
+
+
   return (
     <div>ServiceManagement</div>
   )
