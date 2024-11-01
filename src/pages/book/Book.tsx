bee46957e22d7fad3734aa921a4edcf3e28996bb
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-01 22:09:14 +0600

    create slod booking successfully

--- src/pages/book/Book.tsx
+++ src/pages/book/Book.tsx
@@ -3,6 +3,12 @@ import { TUser, useCurrentUser } from "../../redux/features/auth/authSlice";
 import { useCreateBookingMutation } from "../../redux/features/booking/booking.api";
 import { useAppSelector } from "../../redux/hooks";
 import { toast } from "sonner";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { formatDateToDDMMYYYY } from "../../utils/utils";
+import { Form, Formik, FormikProps } from "formik";
+import Input from "../../components/formik/Input";
+import Dropdown from "../../components/formik/Dropdown";
+import { vehicleOptions } from "../../utils/const.utils";
 
 interface TInitialValues {
   name: string;
@@ -68,7 +74,94 @@ const Book = () => {
   };
 
   return (
-    <div>Book</div>
+    <div className="container pt-[70px]">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-rose-600 Oswald">Secure Your Slot</h1>
+        <p className="text-xl mt-1 Montserrat mb-5">Choose your preferred service slot and complete your booking with ease</p>
+      </div>
+    <div className="grid lg:grid-cols-2 grid-cols-1 lg:gap-x-[70px]">
+      <div>
+        <div className="flex flex-col gap-y-5">
+          <div className="bg-primary-foreground/5 p-5 rounded-md">
+            <h2 className="text-xl font-semibold text-primary-foreground mb-5">
+              Service Details
+            </h2>
+            <h3 className="text-xl font-semibold mb-2">
+              {serviceDetails?.name}
+            </h3>
+            <p className="">{serviceDetails?.description.slice(0, 50)}</p>
+            <div className="flex gap-x-5 w-full mt-2">
+              <p className="font-medium">
+                Duration: {serviceDetails?.duration}
+              </p>
+              <p className="font-medium flex items-center">
+                Price: {serviceDetails?.price} <BsCurrencyDollar />{" "}
+              </p>
+            </div>
+          </div>
+          <div className={`p-5 rounded-md bg-primary-foreground/5`}>
+            <h2 className="text-xl font-semibold text-primary-foreground mb-5">
+              Slot Details
+            </h2>
+            <p className="">
+              Status:{" "}
+              <span className="font-medium capitalize">
+                {slotDetails?.isBooked}
+              </span>
+            </p>
+            <p>
+              Date:{" "}
+              <span className="font-medium">
+                {formatDateToDDMMYYYY(slotDetails.date)}
+              </span>
+            </p>
+            <div className="flex gap-x-5">
+              <p>
+                Start time:{" "}
+                <span className="font-medium">{slotDetails?.startTime}</span>
+              </p>
+              <p>
+                End time:{" "}
+                <span className="font-medium">{slotDetails?.endTime}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+          {({ setFieldValue }: FormikProps<TInitialValues>) => {
+            return (
+              <Form className="space-y-5">
+                <Input name="name" label="Name" readOnly />
+                <Input name="email" label="Email" readOnly />
+                <Input name="time" label="Time" readOnly />
+                <Input name="amount" label="Price" readOnly />
+                <Dropdown
+                  label="Vehicle Type"
+                  name="vehicleType"
+                  options={vehicleOptions}
+                  setFieldValue={setFieldValue}
+                  placeholder="Select vehicle type"
+                />
+                <Input
+                  name="registrationPlate"
+                  label="Registration Plate"
+                  required
+                />
+                <Input name="vehicleBrand" label="Vehicle Brand" />
+                <Input name="vehicleModel" label="Vehicle Model" />
+                <Input name="manufacturingYear" label="Manufacturing Year" />
+                <button type="submit" className="form-submit-btn w-full">
+                  Pay Now
+                </button>
+              </Form>
+            );
+          }}
+        </Formik>
+      </div>
+    </div>
+  </div>
   )
 }
 
