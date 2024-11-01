d4669ea500fcefa4684810250c1a34cec1b831d7
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-01 15:21:11 +0600

    get service data and slot data

--- src/pages/dashboard/Admin/slotManagement/SlotManagement.tsx
+++ src/pages/dashboard/Admin/slotManagement/SlotManagement.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { TSlotWithService } from "../../../../types/redux.type";
+import { useCreateSlotMutation, useGetAllSlotsQuery, useUpdateSlotMutation } from "../../../../redux/features/slot/slots.api";
+import { useGetServicesQuery } from "../../../../redux/features/services/services.api";
+
 const slotStatusOptions = [
   {
     label: "Available",
@@ -27,6 +32,22 @@ const createSlotInitialValues: TCreateSlotInitialValues = {
 };
 
 const SlotManagement = () => {
+  const { data, isLoading } = useGetAllSlotsQuery(undefined);
+  const { data: servicesData } = useGetServicesQuery(undefined);
+  const [updateSlot] = useUpdateSlotMutation();
+  const [createSlot] = useCreateSlotMutation();
+  // modal
+  const [isSlotUpdateModalOpen, setSlotUpdateModalOpen] = useState(false);
+  const [isSlotCreateModalOpen, setSlotCreateModalOpen] = useState(false);
+  // utils
+  const [selectedSlot, setSelectedSlot] = useState<TSlotWithService | null>(
+    null
+  );
+
+  const initialValues = {
+    isBooked: selectedSlot?.isBooked || "",
+  };
+
   return (
     <div>SlotManagement</div>
   )
