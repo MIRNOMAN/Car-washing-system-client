10133a761b2f3e6d40948a393aa0c2a1cb4e083a
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-01 15:17:22 +0600

    create admin slot management

--- src/pages/dashboard/Admin/slotManagement/SlotManagement.tsx
+++ src/pages/dashboard/Admin/slotManagement/SlotManagement.tsx
@@ -1,4 +1,30 @@
+const slotStatusOptions = [
+  {
+    label: "Available",
+    value: "available",
+  },
+  {
+    label: "Cancel",
+    value: "canceled",
+  },
+];
 
+type TInitialValues = {
+  isBooked: string;
+};
+type TCreateSlotInitialValues = {
+  service: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+};
+
+const createSlotInitialValues: TCreateSlotInitialValues = {
+  service: "",
+  date: "",
+  startTime: "",
+  endTime: "",
+};
 
 const SlotManagement = () => {
   return (
