226f7687a28b440a6a07b8e06fdf3dd420e0302c
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-01 16:04:34 +0600

    create user management

--- src/pages/dashboard/Admin/userManagement/UserManagement.tsx
+++ src/pages/dashboard/Admin/userManagement/UserManagement.tsx
@@ -1,4 +1,17 @@
+const userRoleOptions = [
+  {
+    label: "User",
+    value: "user",
+  },
+  {
+    label: "Admin",
+    value: "admin",
+  },
+];
 
+type TInitialValues = {
+  role: string;
+};
 
 const UserManagement = () => {
   return (
