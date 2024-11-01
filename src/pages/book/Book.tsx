19d1cb0dbb41aa8c165e2979099d6d8f173646e5
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-01 22:05:00 +0600

    create select slot,slot details

--- src/pages/book/Book.tsx
+++ src/pages/book/Book.tsx
@@ -1,3 +1,8 @@
+import { useLocation } from "react-router-dom";
+import { TUser, useCurrentUser } from "../../redux/features/auth/authSlice";
+import { useCreateBookingMutation } from "../../redux/features/booking/booking.api";
+import { useAppSelector } from "../../redux/hooks";
+
 interface TInitialValues {
   name: string;
   email: string;
@@ -11,6 +16,27 @@ interface TInitialValues {
 }
 
 const Book = () => {
+  const location = useLocation();
+  const selectedSlot = location.state;
+  const serviceDetails = selectedSlot[0];
+  const slotDetails = selectedSlot[1];
+
+  const userInfo = useAppSelector(useCurrentUser) as TUser;
+
+  const [createBooking] = useCreateBookingMutation();
+
+  const initialValues: TInitialValues = {
+    name: userInfo?.name || "",
+    email: userInfo?.email || "",
+    amount: serviceDetails?.price || "",
+    vehicleType: "",
+    registrationPlate: "ABC420",
+    time: `${slotDetails?.startTime} - ${slotDetails?.endTime}`,
+    vehicleBrand: "RR",
+    vehicleModel: "Galaxy",
+    manufacturingYear: "2023",
+  };
+
   return (
     <div>Book</div>
   )
