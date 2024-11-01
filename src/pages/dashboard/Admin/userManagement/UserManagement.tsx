7023b52f206c51686223e128c575c89e4378bb47
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-01 16:07:51 +0600

    update user management data

--- src/pages/dashboard/Admin/userManagement/UserManagement.tsx
+++ src/pages/dashboard/Admin/userManagement/UserManagement.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
-import { TUserResponse } from "../../../../types/redux.type";
+import { TErrorResponse, TUserResponse } from "../../../../types/redux.type";
 import { useGetAllUsersQuery, useUpdateUserMutation } from "../../../../redux/features/user/user.api";
 import LoadingSpinier from "../../../../components/global/LoadingSpinier";
+import { toast } from "sonner";
+import CustomModal from "../../../../components/reUsable/CustomModal";
+import { Form, Formik, FormikProps } from "formik";
+import Dropdown from "../../../../components/formik/Dropdown";
 
 const userRoleOptions = [
   {
@@ -57,7 +61,89 @@ const UserManagement = () => {
 
 
   return (
-    <div>UserManagement</div>
+    <>
+    <div className="">
+    <div className="text-center">
+        <h1 className="text-3xl font-bold text-rose-600 Oswald">User Management</h1>
+        <p className="text-xl mt-1 Montserrat mb-5">Overview and manage user roles</p>
+      </div>
+      
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white border border-gray-200">
+          <thead>
+            <tr>
+              <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-gray-600 tracking-wider">
+                Name
+              </th>
+              <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-gray-600 tracking-wider">
+                Email
+              </th>
+              <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-gray-600 tracking-wider">
+                Phone
+              </th>
+              <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-gray-600 tracking-wider">
+                Role
+              </th>
+              <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-gray-600 tracking-wider">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.data.map((user: TUserResponse) => (
+              <tr key={user._id} className="hover:bg-gray-100">
+                <td className="px-6 py-4 border-b border-gray-200">
+                  {user.name}
+                </td>
+                <td className="px-6 py-4 border-b border-gray-200">
+                  {user.email}
+                </td>
+                <td className="px-6 py-4 border-b border-gray-200">
+                  {user.phone}
+                </td>
+                <td className="px-6 py-4 border-b border-gray-200">
+                  {user.role}
+                </td>
+                <td className="px-6 py-4 border-b border-gray-200">
+                  <div className="flex space-x-2">
+                    <button
+                      onClick={() => {
+                        setUserInfo(user);
+                        setUserUpdateModalOpen(true);
+                      }}
+                      className="text-white bg-primary-foreground py-1 px-4 rounded-md font-medium"
+                    >
+                      Edit
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <CustomModal
+      isOpen={isUserUpdateModalOpen}
+      setIsOpen={setUserUpdateModalOpen}
+    >
+      <Formik initialValues={initialValues} onSubmit={handleUserUpdate}>
+        {({ setFieldValue }: FormikProps<TInitialValues>) => {
+          return (
+            <Form className="space-y-5">
+              <Dropdown
+                name="role"
+                options={userRoleOptions}
+                setFieldValue={setFieldValue}
+                placeholder="Select role"
+              />
+              <button className="form-submit-btn w-full">Submit</button>
+            </Form>
+          );
+        }}
+      </Formik>
+    </CustomModal>
+  </>
   )
 }
 
